0b143cb1466ea3c64ffc65d2d535ca7037d45933
Neek0tine <77283651+Neek0tine@users.noreply.github.com>
2021-12-19 18:45:56 +0700

    Fixed syntax ig?

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def main():
 
 
 def run():
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33507))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33507)))
 
 def keep_alive():
     server = Thread(target=run)
