6ba3ef58731ac45114295af6533714d38a21bd6a
Neek0tine <nicholas.juan.kalvin-2020@stmm.unair.ac.id>
2022-01-09 00:27:08 +0700

    literallly everything lmao

--- cogs/nukegen.py
+++ cogs/nukegen.py
@@ -22,106 +22,167 @@ class NukegenCog(commands.Cog):
     async def nuke(self, ctx, arg=random.randint(0, 400000)):
         await ctx.trigger_typing()
 
-        _random = ''
-        _img = ''
+        async def _nuke(_arg):
+            _random = ''
+            _img = ''
 
-        try:
-            _random = nhentai.get_doujin(arg)
-            _img = _random.cover
-        except Exception as e:
-            await ctx.channel.send(e)
-            await asyncio.sleep(1)
-            await ctx.channel.send(f'{choice(errors)} <@256713166149386240> {e}')
-            await asyncio.sleep(2)
-            await ctx.channel.send("Meanwhile, enjoy this")
-            await ctx.channel.send(f"https://nhentai.net/g/{random.randint(0, 4000)}")
+            async def again(_arg):
+                try:
+                    _random = nhentai.get_doujin(_arg)
+                    _img = _random.cover
+                    return _random, _img
+                except Exception as e:
+                    # await ctx.channel.send(e)
+                    # await asyncio.sleep(1)
+                    # await ctx.channel.send(f'{random.choice(errors)} <@256713166149386240> {e}')
+                    # await asyncio.sleep(2)
+                    await again(random.randint(0, 400000))
 
-        print(f'[+] Nuke command launched! Gathered ID: {_random.id}')
+            _random, _img = await again(_arg)
 
-        info = dict()
-        for tags in enumerate(_random.tags):
+            print(f'[+] Nuke command launched! Gathered ID: {_random.id}')
 
-            if tags[1].type in info:
-                # append the new number to the existing array at this slot
-                info[tags[1].type].append(tags[1].name)
-            else:
-                # create a new array in this slot
-                info[tags[1].type] = [tags[1].name]
+            info = dict()
+            for tags in enumerate(_random.tags):
 
-        _group = 'None'
-        _language = ''
-        _artist = ''
-        _character = ''
-        _tag = ''
-        _parody = ''
-        _category = ''
+                if tags[1].type in info:
+                    # append the new number to the existing array at this slot
+                    info[tags[1].type].append(tags[1].name)
+                else:
+                    # create a new array in this slot
+                    info[tags[1].type] = [tags[1].name]
 
-        try:
-            _group = [group.title() for group in info['group']]
-            _group = ", ".join(_group)
-        except Exception:
             _group = 'None'
+            _language = ''
+            _artist = ''
+            _character = ''
+            _tag = ''
+            _parody = ''
+            _category = ''
 
-        try:
-            _parody = str("".join(info['parody'])).title()
-        except Exception:
-            _parody = 'Original'
+            try:
+                _group = [group.title() for group in info['group']]
+                _group = ", ".join(_group)
+            except Exception:
+                _group = 'None'
 
-        try:
-            _character = [character.title() for character in info['character']]
-            _character = ", ".join(_character)
-        except Exception:
-            _character = 'Original'
+            try:
+                _parody = str("".join(info['parody'])).title()
+            except Exception:
+                _parody = 'Original'
 
-        try:
-            _artist = [artist.title() for artist in info['artist']]
-            _artist = ", ".join(_artist)
-        except Exception:
-            _artist = 'Unknown'
+            try:
+                _character = [character.title() for character in info['character']]
+                _character = ", ".join(_character)
+            except Exception:
+                _character = 'Original'
 
-        try:
-            _language = [language.title() for language in info['language']]
-            _language = ", ".join(_language)
-        except Exception:
-            _language = 'Unknown'
+            try:
+                _artist = [artist.title() for artist in info['artist']]
+                _artist = ", ".join(_artist)
+            except Exception:
+                _artist = 'Unknown'
 
-        try:
-            _category = [category.title() for category in info['category']]
-            _category = ", ".join(_category)
-        except Exception:
-            _category = 'Hentai'
+            try:
+                _language = [language.title() for language in info['language']]
+                _language = ", ".join(_language)
+            except Exception:
+                _language = 'Unknown'
 
-        try:
-            _tag = [str(f'[{tag.title()}](https://nhentai.net/tags/{str(tag).replace(" ", "-")})') for tag in info['tag']]
-            _tag = ", ".join(_tag)
-            _tag = (_tag[:1000] + '..') if len(_tag) > 1000 else _tag
-        except Exception:
-            _tag = 'None'
+            try:
+                _category = [category.title() for category in info['category']]
+                _category = ", ".join(_category)
+            except Exception:
+                _category = 'Hentai'
 
-        _nhen = Embed(title=_random.titles['english'], description='Here you go, you degenerate', color=0x87CEEB)
-        _nhen.set_thumbnail(url=_img)
-        _nhen.add_field(name="Info", value=f"Parody : {_parody}\nArtist  : {_artist}\nGroup   : {_group}\nLanguage  : {_language}\nCharacters   : {_character}\nCategory    : {_category}")
-        # _nhen.add_field(name="** **", value=f": {_parody}\n: {_artist}\n: {_group}\n: {_language}\n: {_character}\n: {_category}", inline=True)
-        _nhen.add_field(name=f"Sauce", value=f'**{_random.id}**', inline=False)
-        _nhen.add_field(name="Tags", value=_tag, inline=False)
+            try:
+                _tag = [str(f'[{tag.title()}](https://nhentai.net/tags/{str(tag).replace(" ", "-")})') for tag in
+                        info['tag']]
+                _tag = ", ".join(_tag)
+                _tag = (_tag[:1000] + '..') if len(_tag) > 1000 else _tag
+            except Exception:
+                _tag = 'None'
 
-        _invoke_view = [[Button(label='Source', url=f'https://nhentai.net/g/{_random.id}', style=ButtonStyle.url),
-                        Button(label='Preview', custom_id='prev', style=ButtonStyle.green)]]
+            _nhen = Embed(title=_random.titles['english'], description='Here you go, you degenerate', color=0x87CEEB)
+            _nhen.set_thumbnail(url=_img)
+            _nhen.add_field(name="Info",
+                            value=f"Parody : {_parody}\nArtist  : {_artist}\nGroup   : {_group}\nLanguage  : "
+                                  f"{_language}\nCharacters   : {_character}\nCategory    : {_category}")
 
-        _nukegen = await ctx.send(embed=_nhen, components=_invoke_view)
+            # _nhen.add_field(name="** **", value=f": {_parody}\n: {_artist}\n: {_group}\n: {_language}\n: {
+            # _character}\n: {_category}", inline=True)
 
-        def check_button(i: Interaction, button):
-            return i.author == ctx.author and i.message == _nukegen
+            _nhen.add_field(name=f"Sauce", value=f'**{_random.id}**', inline=False)
+            _nhen.add_field(name="Tags", value=_tag, inline=False)
+
+            return _nhen, _random.id
+
+        #
+        # _invoke_view = [[Button(label='Source', url=f'https://nhentai.net/g/{_random.id}', style=ButtonStyle.url),
+        #                 Button(label='Preview', custom_id='prev', style=ButtonStyle.green),
+        #                 Button(label='Randomize', custom_id='rand', style=ButtonStyle.blurple)]]
+
+        # _nukegen = await ctx.send(embed=_nhen, components=_invoke_view)
+
+        _link = ''
+        _em, _link = await _nuke(arg)
+        _nukegen = await ctx.send(embed=_em)
+        await ctx.message.delete()
+
+        _prev = '🔎'
+        _rand = '🎲'
+        _pin = '📌'
+        _close = '❌'
+
+        await _nukegen.add_reaction(_prev)
+        await _nukegen.add_reaction(_rand)
+        await _nukegen.add_reaction(_pin)
+        await _nukegen.add_reaction(_close)
+
+        _guide = await ctx.send("🔎 Read  |  🎲 Randomize  |  📌 Save")
+
+        _valid_reactions = ['🔎', '🎲', '📌', '❌']
+        timeout = time.time() + 60 * 2  # 5 minutes from now
 
         while True:
-            timeout = time.time() + 60*2  # 5 minutes from now
-            interaction, button = await self.bot.wait_for('button_click', check=check_button)
 
             if time.time() > timeout:
+                await ctx.send('Ah i forgot the link, you took too long')
                 break
+
+            def check_react(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in _valid_reactions
+
+            reaction, user = await self.bot.wait_for('reaction_add', check=check_react)
+
+            timeout = time.time() + 60 * 2  # 5 minutes from now
+            if str(reaction.emoji) == _prev:
+                await _nukegen.remove_reaction(_prev, user)
+                await _nukegen.delete()
+                await _guide.delete()
+                await ctx.invoke(self.bot.get_command('preview'), arg=_link)
+
+            elif str(reaction.emoji) == _close:
+                await _nukegen.delete()
+                await _guide.delete()
+
+            elif str(reaction.emoji) == _rand:
+                await _nukegen.remove_reaction(_rand, user)
+                while True:
+                    try:
+                        _em, _link = await _nuke(random.randint(0, 400000))
+                        await _nukegen.edit(embed=_em)
+                        break
+                    except:
+                        pass
+
+            elif str(reaction.emoji) == _pin:
+                async for user in reaction.users():
+                    await _nukegen.remove_reaction(_pin, user)
+                    await user.send(f'https://nhentai.net/g/{_link}')
+
             else:
-                await ctx.invoke(self.bot.get_command('preview'), arg=_random.id)
-                break
+                await ctx.send('How the fuck did you get here?')
 
 
 def setup(bot):
--- cogs/previewer.py
+++ cogs/previewer.py
@@ -1,3 +1,4 @@
+import discord.errors
 import requests
 import nhentai
 import asyncio
@@ -13,70 +14,158 @@ class PreviewerCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def preview(self, ctx, arg=259049):
-        _page = 1
-        _dojin = nhentai.get_doujin(arg)
-        _tot_page = len(_dojin.pages)
+    async def preview(self, ctx, arg='259049', req_page=1):
 
-        def get_img(_code, _page):
-            return str(
-                (BeautifulSoup(
-                    (get(f'https://nhentai.net/g/{_code}/{_page}')).content, 'html.parser')
-                ).find_all('img', src=True)[-1]['src'])
+        _page = ''
+        _dojin = ''
+        _tot_page = ''
 
-        _view = Embed(title=f'{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
+        if str(arg).startswith('http'):
+            _sparg = str(arg).split(sep='/')
+            print(_sparg)
+
+            if len(_sparg) == 5:
+                if _sparg[-1] == '':
+                    print('[!] Are you sure it\'s the id is correct?')
+                    await ctx.send('is the id correct?')
+                elif _sparg[-1] != '':
+                    arg = int(_sparg[-1])
+                    _page = 1
+                    _link = str("/".join(_sparg)) + '/' + str(_page)
+                    print(arg, _link, _page)
+
+            elif len(_sparg) == 6:
+                if _sparg[-1] == '':
+                    _page = 1
+                    _link = str("/".join(_sparg)) + str(_page)
+                else:
+                    arg = int(_sparg[-2])
+                    _page = _sparg[-1]
+                    _link = str("/".join(_sparg))
+                    print(arg, _link, _page)
+
+            elif len(_sparg) < 5 or len(_sparg) > 7:
+                print('[!] Are you sure it\'s the correct link?')
+
+            arg = int(arg)
+            _page = int(_page)
+            _dojin = nhentai.get_doujin(arg)
+            _tot_page = len(_dojin.pages)
+
+        else:
+            _page = req_page
+            _dojin = nhentai.get_doujin(arg)
+            _tot_page = len(_dojin.pages)
+
+        def get_img(_code, _page, _url=None):
+            if _url is not None:
+                return str(
+                    (BeautifulSoup(
+                        (get(f'{_url}')).content, 'html.parser')
+                    ).find_all('img', src=True)[-1]['src'])
+            else:
+                return str(
+                    (BeautifulSoup(
+                        (get(f'https://nhentai.net/g/{_code}/{_page}')).content, 'html.parser')
+                    ).find_all('img', src=True)[-1]['src'])
+
+        _view = Embed(title=f'[{arg}]{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
         _view.set_image(url=get_img(arg, _page))
+        try:
+            await ctx.message.delete()
+        except discord.errors.NotFound:
+            pass
 
-        _view_comp =[[Button(label='Source', url=f'https://nhentai.net/g/{arg}', style=ButtonStyle.url),
-                      Button(label='Previous', custom_id='prev', style=ButtonStyle.green),
-                      Button(label='Next', custom_id='next', style=ButtonStyle.green)]]
+        _interactive_buttons = await ctx.send(embed=_view)
 
-        _interactive_buttons = await ctx.send(embed=_view, components=_view_comp)
+        _next_re = '▶'
+        _prev_re = '◀'
+        _pin = '📌'
+        _close = '❌'
+        _valid_reactions = ['▶', '◀', '📌', '❌']
 
-        def check_button(i: Interaction, button):
-            return i.author == ctx.author and i.message == _interactive_buttons
+        _guide = await ctx.send(" ◀ Previous  |  ▶ Next  |  📌 save")
+        await _interactive_buttons.add_reaction('◀')
+        await _interactive_buttons.add_reaction('▶')
+        await _interactive_buttons.add_reaction('📌')
+        await _interactive_buttons.add_reaction('❌')
+        timeout = time.time() + 60 * 2  # 5 minutes from now
 
         while True:
-            timeout = time.time() + 60*2  # 5 minutes from now
-            interaction, button = await self.bot.wait_for('button_click', check=check_button)
 
             if time.time() > timeout:
-                await ctx.send('Ah i forgot the link, you took too long')
                 break
 
-            elif _page == 1:
-                if button.custom_id == 'next':
-                    _page += 1
-                    _view = Embed(title=f'{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
-                    _view.set_image(url=get_img(arg, _page))
-                    await interaction.edit(embed=_view)
+            done_tasks = None
+            check_react = lambda reaction, user: user == ctx.author and str(reaction.emoji) in _valid_reactions
+            check_button = lambda interaction, button: interaction.author == ctx.author and interaction.message == _interactive_buttons
 
+            pending_tasks = [self.bot.wait_for('button_click', check=check_button),
+                             self.bot.wait_for('reaction_add', check=check_react)]
+
+            done_tasks, pending_tasks = await asyncio.wait(pending_tasks, return_when=asyncio.FIRST_COMPLETED)
+
+            for task in pending_tasks:
+                task.cancel()
+
+            timeout = time.time() + 60 * 2  # 5 minutes from now
+            for task in done_tasks:
+                reaction, user = await task
+                interaction, button = await task
+
+                if reaction and user:
+                    if time.time() > timeout:
+                        await ctx.send('Ah i forgot the link, you took too long')
+                        break
+
+                    elif str(reaction.emoji) == _pin:
+                        await _interactive_buttons.remove_reaction('📌', user)
+                        await user.send(f'https://nhentai.net/g/{arg}/{_page}')
+
+                    elif str(reaction.emoji) == _close:
+                        await _interactive_buttons.delete()
+                        await _guide.delete()
+
+                    elif _page == 1:
+                        if str(reaction.emoji) == _next_re:
+                            await _interactive_buttons.remove_reaction('▶', user)
+                            _page += 1
+                            _view = Embed(title=f'[{arg}]{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
+                            _view.set_image(url=get_img(arg, _page))
+                            await _interactive_buttons.edit(embed=_view)
+
+                        else:
+                            await _interactive_buttons.remove_reaction('◀', user)
+
+                    elif _page == _tot_page:
+                        if str(reaction.emoji) == _prev_re:
+                            await _interactive_buttons.remove_reaction('◀', user)
+                            _page -= 1
+                            _view = Embed(title=f'[{arg}]{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
+                            _view.set_image(url=get_img(arg, _page))
+                            await _interactive_buttons.edit(embed=_view)
+
+                        else:
+                            await _interactive_buttons.remove_reaction('◀', user)
+
+
+
+                    else:
+                        if str(reaction.emoji) == _prev_re:
+                            await _interactive_buttons.remove_reaction('◀', user)
+                            _page -= 1
+                            _view = Embed(title=f'[{arg}]{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
+                            _view.set_image(url=get_img(arg, _page))
+                            await _interactive_buttons.edit(embed=_view)
+
+                        elif str(reaction.emoji) == _next_re:
+                            await _interactive_buttons.remove_reaction('▶', user)
+                            _page += 1
+                            _view = Embed(title=f'[{arg}]{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
+                            _view.set_image(url=get_img(arg, _page))
+                            await _interactive_buttons.edit(embed=_view)
                 else:
-                    await ctx.send('there\'s no page 0')
-
-            elif _page == _tot_page:
-                if button.custom_id == 'next':
-                     await ctx.send('what are you trying to read? the doujin\'s over.')
-
-                else:
-                    _page += 1
-                    _view = Embed(title=f'{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
-                    _view.set_image(url=get_img(arg, _page))
-                    await interaction.edit(embed=_view)
-
-            else:
-                if button.custom_id == 'next':
-                    _page += 1
-                    _view = Embed(title=f'{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
-                    _view.set_image(url=get_img(arg, _page))
-                    await interaction.edit(embed=_view)
-
-                else:
-                    _page -= 1
-                    _view = Embed(title=f'{_dojin.titles["english"]} page {_page}', color=0x87CEEB)
-                    _view.set_image(url=get_img(arg, _page))
-                    await interaction.edit(embed=_view)
-
+                    ctx.send('How did you get here?')
 
 
 def setup(bot):
